6517a8ffa9fd578a67902bee4027dadee4f4e2fc
Jack Huang <719668276@qq.com>
2022-03-30 17:48:44 +0800

    update

--- tutorial/1_play_beats.py
+++ tutorial/1_play_beats.py
@@ -1,14 +1,5 @@
 import simpleaudio, time 
-
 strong_beat = simpleaudio.WaveObject.from_wave_file('strong_beat.wav')
-
-count = 0
-interval = 0.5
-while count < 4:
+while True:
     strong_beat.play()
-    count = count + 1
-    time.sleep(interval)
-
-
-weak_beat = simpleaudio.WaveObject.from_wave_file('weak_beat.wav')
-weak_beat.play()
+    time.sleep(0.5)
